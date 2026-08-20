6e5c7252cbd1e7d0145fee158ce9ef5e7c2dc412
reediscool8 <reedthefarmer1@gmail.com>
2026-08-20 17:03:06 +0000

    moores

---
+++ moores.py
@@ -0,0 +1,10 @@
+def main():
+
+    Transistors = 17000000000
+
+    years = int(input(" How many years?"))
+
+    print("Transtors now", Transistors * 2 **(years / 2))
+
+if __name__ == "__main__":
+        main()
