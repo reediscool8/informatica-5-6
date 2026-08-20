cf653a2b74bcf7b6b05d0e87b8f11160786c8226
reediscool8 <reedthefarmer1@gmail.com>
2026-08-20 16:40:04 +0000

    stuf

---
+++ update-variables.py
@@ -0,0 +1,31 @@
+def main():
+    #Augmented Assignment Operator
+    money = 5
+    money = money += 10
+    print(money) # This will print 15
+
+    #Subtraction Assignmrnt Operator
+    minutes = 60
+    minutes -= 25
+    print(minutes) #This will print 35
+
+    # Multiplication Assignment Operator
+    num = 10
+    num *= 4
+    print(num) #This will give 40
+
+    # Division Assignment Operator
+
+    pizzas = 8
+    pizzas /= 4
+    print(pizzas)
+
+    # Modulus Assignment Operator
+    num1 = 10
+    num2 = 5
+    num1 %= num2
+    print(num1)
+
+
+if __name__ == "__name__" :
+    main()
