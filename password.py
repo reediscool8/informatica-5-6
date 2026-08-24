78c2d20635745ea7fcb6ffdd1ad03adda6e80023
reediscool8 <reedthefarmer1@gmail.com>
2026-08-24 19:36:04 +0000

    password

---
+++ password.py
@@ -0,0 +1,11 @@
+def main():
+    password = "Soyelcachorromasguapo"
+
+    attempt = input("Enter Password: ")
+    if attempt == password:
+        print("good boy")
+
+    print("Bad Boy!")
+
+if __name__ == "__main__":
+    main()
