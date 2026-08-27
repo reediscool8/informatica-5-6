56a9f86ce5caaae84b2e741a489cef0036716b76
reediscool8 <reedthefarmer1@gmail.com>
2026-08-27 17:14:56 +0000

    the end

--- atmosphere.py
+++ atmosphere.py
@@ -15,18 +15,34 @@ def main():
         print("Check your spelling")
 
 
-    altitude = float(input("Enter exact altitude"))
+    altitude = float(input("Enter exact altitude:  "))
 
     if altitude > 10000:
         time = 4650 + 1230 + 175 + 506.7 + 600
+        print("Total descent time: ")
+        print(time)
     elif altitude > 700:
-        time = (((altitude - 700) / 2) + 1230 + 175 + 506.7 + 600)
-
-
-
-
-
-
+        time = (((altitude - 700) * 1000 / 2000) + 1230 + 175 + 506.7 + 600)
+        print("Total descent time: ")
+        print(time)
+    elif altitude > 85:
+        time = (((altitude - 85) * 1000 / 500) + 175 + 506.7 + 600)
+        print("Total descent time: ")
+        print(time)
+    elif altitude > 50:
+        time = (((altitude - 50) * 1000 / 200) + 506.7 + 600)
+        print("Total descent time: ")
+        print(time)
+    elif altitude > 12:
+        time = (((altitude - 12) * 1000 / 75) + 600)
+        print("Total descent time: ")
+        print(time)
+    elif altitude > 0:
+        time = (altitude * 1000 / 20)
+        print("Total descent time: ")
+        print(time)
+    else:
+        print("sorry i cant help you")
 
 
 if __name__ == "__main__":
