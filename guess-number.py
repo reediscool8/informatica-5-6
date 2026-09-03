b9110fc0b34b23e0445b368e093685098ed54f7d
reediscool8 <reedthefarmer1@gmail.com>
2026-09-03 20:17:26 +0000

    dfdsg

--- guess-number.py
+++ guess-number.py
@@ -1,26 +1,22 @@
 import random
 
 def main():
+    number = random.randint(1, 100)
+    name = input("Hello! What is your name? ")
 
-    number = ""
-
-    number = random.randint(1,100)
-
-
-    name = input("Hello! What is your name?")
-    guess = (f"Well, {name}, I am thinking of a number between 1 and 100. Take a guess:  ")
+    guess = int(input(f"Well, {name}, I am thinking of a number between 1 and 100. Take a guess: "))
 
     while guess != number:
         if number > guess:
-            print("Your guess is to low Guess again")
-        if number < guess:
-            print("Your guess was to high Guess again")
-        if number == guess:
-            break
+            print("Your guess is too low. Guess again.")
 
+        elif number < guess:
+            print("Your guess was too high. Guess again.")
+
+
+        guess = int(input("Take another guess: "))
+
+    print(f"You guessed it!  Gooooood boy {name}")
 
 if __name__ == "__main__":
     main()
-
-
-
